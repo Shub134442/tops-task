7ee235960d0df5710cd6dc2e30aaadb789a201a0
shubham kotadiya <102743021+Shub134442@users.noreply.github.com>
2026-03-20 18:49:04 +0530

    Delete assignment/Module 2 directory

--- assignment/Module 2/Operators.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() 
-{
-    int a=5,b=3;
-    printf("%d", a+b);
-}
--- assignment/Module 2/Pointer.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a=10;
-    int *p=&a;
-    *p=20;
-    
-    printf("%d",a);
-}
