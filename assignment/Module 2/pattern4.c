c3a26f671e7383a3517379b6d132c21081a6bec8
shubham kotadiya <102743021+Shub134442@users.noreply.github.com>
2026-03-29 16:50:01 +0530

    Add files via upload

---
+++ assignment/Module 2/pattern4.c
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main() {
+  int rows = 5;
+  int num = 1;
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      printf("%d ", num);
+      num++;
+    }
+    printf("\n");
+  }
+  return 0;
+}
